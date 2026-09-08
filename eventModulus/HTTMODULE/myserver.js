001e434213a52bcb29d59c4fc99e475f4a49a0ae
RPS2610 <rudra.25b01010986@abes.ac.in>
2026-09-08 10:55:19 +0530

    API

--- eventModulus/HTTMODULE/myserver.js
+++ eventModulus/HTTMODULE/myserver.js
@@ -1,13 +1,25 @@
-//create my own server  using http module
+// //create my own server  using http module
 
-import http from "http";
+// import http from "http";
 
-const server = http.createServer((req,res)=>{
-res.write("welcome to my server");
-res.end();
-})
+// const server = http.createServer((req,res)=>{
+// res.write("welcome to my server");
+// res.end();
+// })
 
 
-server.listen(8000,()=>{
-    console.log("server is running on port 8000");
-})
+// server.listen(8000,()=>{
+//     console.log("server is running on port 8000");
+// })
+
+//create my own server using http module
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.write("welcome to my server");
+    res.end();
+});
+
+server.listen(8000, () => {
+    console.log("server is running on http://localhost:8000");
+});
